97aa4e8f71a5ad4c0e83559ec2886e4684851135
Vyse1986 <vedhasdoshi@gmail.com>
2025-06-24 09:45:30 -0400

    Finished chapter 14

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -129,7 +129,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 							))}
 					</div>
 				</fieldset>
-				<div id='customer-error' aria-live='polite' aria-atomic='true'>
+				<div id='state-error' aria-live='polite' aria-atomic='true'>
 					{state.errors && <p className='mt-2 text-sm text-red-500'>{state.message}</p>}
 				</div>
 			</div>
